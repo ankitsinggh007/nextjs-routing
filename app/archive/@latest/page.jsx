a315d5e38d71ebd773b9e87edd2daab6f3cd7820
Ankit singh <as3824115@gmail.com>
2024-11-15 00:35:07 +0530

    resolved bug

--- app/archive/@latest/page.jsx
+++ app/archive/@latest/page.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import NewsList from '@/component/news-list';
+import { getLatestNews } from '@/lib/news';
 
 function Latestpage() {
+  const news=getLatestNews();
+
   return (
     <>
     <div>latest page</div>
