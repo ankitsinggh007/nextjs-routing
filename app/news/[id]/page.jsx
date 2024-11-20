d3f14cdf0f2737302a9b976517c07e573d8c5829
Ankit singh <as3824115@gmail.com>
2024-11-20 21:47:34 +0530

    ready to implement intercepted route

--- app/news/[id]/page.jsx
+++ app/news/[id]/page.jsx
@@ -1,8 +1,10 @@
 import { DUMMY_NEWS } from '@/dummy-news'
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import React from 'react'
 
 function page({params}) {
+  console.log(params)
 
 
 
@@ -14,7 +16,7 @@ if(!data) return notFound();
   return (
     <article className='news-article'>
       <header>
-        <img src={`/images/news/${data.image}`} alt={data.title}/>
+      <Link href={`/news/${data.slug}/image`}>  <img src={`/images/news/${data.image}`} alt={data.title}/></Link>
         <h1>{data.title}</h1>
         <time dateTime={data.date}  >{data.date}</time>
       </header>
